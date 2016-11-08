f0c164c6698a1fe7e7fe264f9f73be842ac6e872
kfirb <kfirb@wix.com>
2016-11-08 20:02:45 +0200

    added cake pattern

---
+++ src/test/scala/com/wix/talks/fp/CakePattern.scala
@@ -0,0 +1,37 @@
+package com.wix.talks.fp
+
+import java.util.UUID
+
+
+
+object CakePattern {
+  val u = new UsersServiceImpl with UsersDaoMySql
+}
+
+
+
+case class User(id: UUID, email: String)
+
+trait UsersDao {
+  def byId(id: UUID): User
+}
+
+trait UsersDaoMySql extends UsersDao{
+  override def byId(id: UUID): User = ??? // bring from database
+}
+
+trait UsersService {
+  def emailById(id: UUID): String
+}
+
+trait UsersServiceImpl extends UsersService {
+  self: UsersDao =>
+
+  override def emailById(id: UUID): String = {
+    byId(id).email
+  }
+}
+
+
+
+
